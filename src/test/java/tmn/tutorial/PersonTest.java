8525b8fc18b1b8021b5e32d98aa854b1aa228b49
tmngassa <tmngassa@hotmail.com>
2014-07-10 20:48:55 -0700

    First commit

---
+++ src/test/java/tmn/tutorial/PersonTest.java
@@ -0,0 +1,14 @@
+package tmn.tutorial;
+
+import org.junit.Test;
+
+import tmn.tutorial.Person;
+import static org.junit.Assert.*;
+
+public class PersonTest {
+    @Test
+    public void canConstructAPersonWithAName() {
+        Person person = new Person("Larry");
+        assertEquals("Larry", person.getName());
+    }
+}
